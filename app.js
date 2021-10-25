e4d19ca477263ba1b6e6d0ad36de6ecb46906108
DanteDangeli <dandangeli@gmail.com>
2021-10-25 20:33:03 -0300

    agregando base del formulario

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const path = require('path');
+
+const methodOverride = require('method-override');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, '/views'));
+
+app.use(express.static(path.join(__dirname,'../public')));
+
+app.use(express.urlencoded({extended:false}));
+
+app.use(express.json());
+
+app.use(methodOverride('_method'));
+
+app.listen(3000, () => {
+    console.log('Servidor andando')
+});
