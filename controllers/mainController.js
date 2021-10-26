87d12051bcdd3327fe64ba4decbf8cc32af6da62
EnsayistaPop <pablo.producer21@gmail.com>
2021-10-25 19:21:19 -0500

    subo controller de elimi

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -8,6 +8,18 @@ const users = JSON.parse(fs.readFileSync(usersPath, 'utf-8'));
 const controller = {
     listarUsuarios : (req, res)=>{
         res.render('listarUsuarios', {users})
+    },
+    eliminarUsuario: (req, res)=>{
+        let userEmail = req.params.email;
+		let userAelim = users.find(user => user.email == userEmail);
+		console.log(userAelim);
+		let userIndice = users.indexOf(userAelim);
+		console.log('el USUARIO se encuentra en el indice:', userIndice);
+		products.splice(userIndice);
+		let usersDelJSON = JSON.stringify(users);
+		fs.writeFileSync(usersPath, usersDelJSON);
+		console.log('USUARIO eliminado :(')
+		res.redirect('/')
     }
 };
 
--- routes/main.js
+++ routes/main.js
@@ -4,6 +4,7 @@ let router = express.Router();
 let mainController = require('../controllers/mainController');
 
 router.get('/listar-usuarios', mainController.listarUsuarios);
+router.delete('/eliminarUsuario', mainController.eliminarUsuario);
 
 
 module.exports=router;
